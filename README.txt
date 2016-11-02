4b6ba381d1a5817993ecb17c60f98b3c605b82a2
Jason Van Kerkhoven <jason.vk@outlook.com>
2016-11-01 21:30:33 -0400

    v1.0.0 - read file work
    reading files works, printing works, going home

---
+++ README.txt
@@ -0,0 +1,45 @@
+Assignment 2
+SYSC 4001
+
+Jason Van Kerkhoven		[100974276]
+Brydon Gibson			[100xxxxxx]
+
+01/11/2016
+
+
+-----------------------------------------------------------
+INPUT FORMAT:
+
+	It should be noted that each line denotes a new proccess with the
+	information per being shown as follows:
+	
+	PID		requiredCPUTime		ioFrequency		ioDuration		ArivalTime
+	
+	With the use of SINGLE SPACES (' ') to denote where one parameter ends,
+	and the next one begins. The values entered are to be encoded with standard
+	8 bit ASCII numerical values, 0-9. The values entered are assumed to be
+	a decimal number during runtime. The values for "requiredCPUTime", 
+	"ioFrequency", "ioDuration" and "ArivalTime" are assumed to be in milliseconds.
+	An example of a process with a PID of 12, a required CPU time of 34ms,
+	a I/O frequency of 56ms, and an I/O duration of 78ms  arriving at a time
+	of 9ms after the simulation starts would be saved as:
+	
+	12 34 56 78 9
+	
+	Multiple processes are done in a similar fashion, with a new line between
+	each process, such that:
+	
+	process_1	|	12 34 56 78 9
+	process_2	|	5 7 10 100 0
+	process_3	|	440 55 66 77 11
+	process_4	|	45 69 87 10 50
+	  ...		|		...
+	process_40	|	112 28 2 25 12
+	
+	Additionally, please note that zero padding any number (ie writting
+	5 as 05 or 0005) is not reccomended nor guarenteed to maintain functionality.
+	There is a maximum number of allowed processes, with the max being 40.
+	Exceding the maximun number of allowed processes will cause unexpected errors.
+	
+	It should also be noted each parameter field has a maximum number of characters
+	of TEN (10) characters.
---
+++ main.c
@@ -0,0 +1,224 @@
+/**
+*Class:             linkedList.c
+*Project:           SYSC4001 Assignment 2
+*Authors:           Jason Van Kerkhoven
+* 					Brydon Gibson                                            
+*Date of Update:    01/11/2016                                              
+*Version:           1.0.0                                                   
+*                                                                                   
+*Purpose:           Stuff
+* 
+* 
+*Update Log:		v1.0.0
+*						- linked list implimented
+*/
+//	printf("%s\n","test");
+
+
+//import external libraries
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PCB_PARAM_CHAR 10
+#define MAX_PROCESSES 40
+
+
+//Declaring data structures and related 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//Process Control Block data struct
+typedef struct PCB
+{
+	int PID;					//ID
+	int requiredCPUTime;		//How many ticks the process has until completion
+	int ioFrequency;			//How often the process goes to do I/O
+	int ioDuration;				//How long the process is gone to do I/O
+	int ioRemaining;			//How many ticks left until I/O is complete
+} PCB;
+
+typedef struct PCBAndArrival
+{
+	PCB pcb;					//the PCB stored
+	int arrival;				//when the PCB is schedualed to arrive
+}PCBAndArrival;
+
+
+
+//Declaring global variables
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+long unsigned int simTime = 0;
+
+PCB readyArr[MAX_PROCESSES];
+int readyArrSize = 0;
+PCBAndArrival newArr[MAX_PROCESSES];
+int newArrSize = 0;
+PCB waitingArr[MAX_PROCESSES];
+int waitingArrSize = 0;
+PCB terminatedArr[MAX_PROCESSES];
+int terminatedArrSize = 0;
+PCB running;
+
+
+
+
+//Various print statments
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//print PCB
+void printPCB(PCB proc)
+{
+	printf("PID: %d\nCPU Time: %d\nIO Freq: %d\nIO Dur: %d\nIO Rem: %d\n",
+			proc.PID,
+			proc.requiredCPUTime,
+			proc.ioFrequency,
+			proc.ioDuration,
+			proc.ioRemaining);
+}
+
+
+//print PCB array
+void printPCBArray(PCB arr[], int length)
+{
+	int i=0;
+	for(i=0; i<length; i++)
+	{
+		//print PCB
+		printPCB(arr[i]);
+	}
+}
+
+
+//print PCBAndArrival array (AKA the most unwieldy name)
+void printPCBAndArrivalArray(PCBAndArrival arr[], int length)
+{
+	int i=0;
+	for(i=0; i<length; i++)
+	{
+		//print PCB
+		printPCB(arr[i].pcb);
+		//print arrival
+		printf("ARV: %d\n\n", arr[i].arrival); 
+	}
+}
+
+
+
+
+
+//Reads a .dat file, fills readyArr with relevant data.
+//returns number of processes read
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int getPCBData(char fileName[])
+{
+	//declaring local variables
+	int numProc = 1;
+	int charRead = 0;
+	FILE *fp;
+	char param[MAX_PCB_PARAM_CHAR];
+	int i = 0;
+	int tempNum = 0;
+	int paramNum = 0;
+	int processNum = 0;
+	
+	//open the .dat file
+	fp = fopen(fileName, "r");
+	//check fp for validity
+	if (fp == NULL)
+	{
+		//error
+		printf("!UNEXPECTED ERROR - FILE NOT FOUND!\n");
+		exit(0);
+	}
+	
+	//read new char
+	charRead = fgetc(fp);
+	printf("Starting newProc...\n");
+	printf("New char read as: %c\n", charRead);
+	
+		//read .dat file until end
+		while(charRead != EOF)
+		{
+			//new param, as a space was found
+			if (charRead == ' ' || charRead == '\n')
+			{
+				//convert current param and save to current process
+				tempNum = atoi(param);
+				printf("param =");
+				for(i=0;param[i] != '\0';i++)
+				{
+					printf(" [%c]", param[i]);
+				}
+				printf("\n");
+				printf("num = %d\n", tempNum);
+				switch(paramNum)
+				{
+					case(0):
+						newArr[processNum].pcb.PID = tempNum;
+						break;
+					case(1):
+						newArr[processNum].pcb.requiredCPUTime = tempNum;
+						break;
+					case(2):
+						newArr[processNum].pcb.ioFrequency = tempNum;
+						break;
+					case(3):
+						newArr[processNum].pcb.ioDuration = tempNum;
+						newArr[processNum].pcb.ioRemaining = 0;
+						break;
+					case(4):
+						newArr[processNum].arrival = tempNum;
+						break;
+					default:
+						printf("ERROR - UNEXPECTED NUMBER OF PARAMETERS");
+						exit(0);
+						break;
+				}
+				
+				//reset i and param, inc paramNum (temporarily hijacking i for a for loop)
+				//(temporarily hijacking i for a for loop)
+				for(i=0; i < MAX_PCB_PARAM_CHAR; i++)
+				{
+					param[i] = '\0';
+				}
+				i=0;
+				paramNum++;
+				
+				//if new line, inc processNum
+				if(charRead == '\n')
+				{
+					//reset i and paramNum, inc processNum and procNum
+					printf("Starting newProc...\n");
+					paramNum = 0;
+					processNum++;
+					numProc++;
+				}
+			}
+			//add to current param
+			else
+			{
+				param[i] = charRead;
+				i++;
+			}
+
+			//read new char
+			charRead = fgetc(fp);
+			printf("New char read as: %c\n", charRead);
+		}
+	return (numProc-1);
+	
+}
+
+
+
+
+//main function
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int main(int argc, char const *argv[])
+{
+	newArrSize = getPCBData("PCBdata.dat");
+	printf("%d PCBs added to NEW array\n", newArrSize);
+	printf("Dumping contents of NEW array...\n");
+	printf("-----------------------------------------\n");
+	printPCBAndArrivalArray(newArr, newArrSize);
+	printf("-----------------------------------------\n");
+}
