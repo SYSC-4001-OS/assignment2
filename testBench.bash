99433a7372e82186939fbcbe1ab9dec05a2dc415
packocrayons <gibson.brydon@gmail.com>
2016-11-04 15:55:04 -0400

    finished testBench

--- testBench.bash
+++ testBench.bash
@@ -51,12 +51,24 @@ echo -e $IOPCBDATA > PCBdata.fcfsx
 
 echo -e "Data written, starting ./fcfsSchedulerIO\n\n"
 else
-echo -e "Using data from PCBData.fcfsx"
+echo -e "Using data from PCBData.fcfsx, starting ./fcfsSchedulerIO\n\n"
 fi
 
 ./fcfsSchedulerIO #run the IO one
 
+if $write ; then
 NONIOPCBDATA="1 22 4 0\n2 11 6 9\n3 12 3 12\n4 13 12 11\n5 14 8 17\n6 70 10 40\n10 2 45 15"
+
+echo -e "\n\nwriting PCB data to PCBdata.dat\n"
+
+echo -e $NONIOPCBDATA > PCBdata.dat
+
+echo -e "Data written, starting ./priorityQueueScheduler\n\n"
+else
+echo -e "Using data from PCBData.dat, starting ./priorityQueueScheduler\n\n"
+fi
+
+./priorityQueueScheduler
 
 if $write ; then
 IOPCBDATA="1 40 10 2 1 10\n2 100 20 10 2 0\n3 25 5 2 3 0\n10 50 12 6 10 0\n12 20 10 50 5 0\n8 25 10 12 5 0"
