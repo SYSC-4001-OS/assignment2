7cce92236ed9251e3d28ab02bc099b873cfa9a47
packocrayons <gibson.brydon@gmail.com>
2016-11-04 13:05:24 -0400

    testbench working, needs one more section for priorityQueueScheduler(non IO)

--- testBench.bash
+++ testBench.bash
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-NONIOPCBDATA="1 22 0
-2 11 9
-3 12 12
-4 13 11
-5 14 17
-6 70 40
-10 2 15"
+NONIOPCBDATA="1 22 0\n2 11 9\n3 12 12\n4 13 11\n5 14 17\n6 70 40\n10 2 15"
 
-echo "writing PCB data to PCBdata.fcfs\n"
+echo -e "writing PCB data to PCBdata.fcfs\n"
 
-echo $NONIOPCBDATA >> PCBdata.fcfs
+echo -e $NONIOPCBDATA > PCBdata.fcfs
 
-echo "Data written, starting ./fcfsScheduler\n\n"
+echo -e "Data written, starting ./fcfsScheduler\n\n"
 
 ./fcfsScheduler #run it, must be executable
 
-IOPCBDATA="1 22 10 1 0
-2 12 8 2 10
-4 15 5 1 45
-10 100 20 8 80
-12 8 5 1 20"
+IOPCBDATA="1 22 10 1 0\n2 12 8 2 10\n4 15 5 1 45\n10 100 20 8 80\n12 8 5 1 20\n"
 
-echo "\n\nwriting PCB data to PCBdata.fcfsx\n"
+echo -e "\n\nwriting PCB data to PCBdata.fcfsx\n"
 
-echo $IOPCBDATA >> PCBdata.fcfsx
+echo -e $IOPCBDATA > PCBdata.fcfsx
 
-echo "Data written, starting ./fcfsSchedulerIO\n\n"
+echo -e "Data written, starting ./fcfsSchedulerIO\n\n"
 
 ./fcfsSchedulerIO #run the IO one
 
-IOPCBDATA="1 40 10 2 1 10
-2 100 20 10 2 0
-3 25 5 2 3 0
-10 50 12 6 10 0
-12 20 10 50 5 0
-8 25 10 12 5 0"
+IOPCBDATA="1 40 10 2 1 10\n2 100 20 10 2 0\n3 25 5 2 3 0\n10 50 12 6 10 0\n12 20 10 50 5 0\n8 25 10 12 5 0"
 
-echo "\n\nwriting PCB data to PCBdata.datx\n"
+echo -e "\n\nwriting PCB data to PCBdata.datx\n"
 
-echo $IOPCBDATA >> PCBdata.fcfsx
+echo -e $IOPCBDATA > PCBdata.fcfsx
 
-echo "Data written, starting ./prioritySchedulerIO\n\n"
+echo -e "Data written, starting ./prioritySchedulerIO\n\n"
 
 ./priorityQueueSchedulerIO
